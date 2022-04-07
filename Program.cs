19752766d41c943913f156fb95169f6366c790a0
SdangondLT <sdangond@lean-tech.io>
2022-04-07 17:53:56 -0500

    added prime number code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace numberPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Escribe un numero");
+            number = int.Parse(Console.ReadLine());
+            if (IsPrime(number) == true)
+                Console.WriteLine($"El {number} es un numero Primo");
+            else
+                Console.WriteLine($"El {number} no es un numero Primo");
+
+            //Console.WriteLine(IsPrime(number));
+        }
+
+        public static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
